98c774da463061d3f3283e3e4b125d517de9ca55
BikutaDesu <victor.neves4@fatec.sp.gov.br>
2020-12-28 15:42:59 -0300

    Ajustes Abas e AppiumRobot

--- src/main/java/br/com/treinamento/etech/commons/AppiumRobot.java
+++ src/main/java/br/com/treinamento/etech/commons/AppiumRobot.java
@@ -1,18 +1,26 @@
 package br.com.treinamento.etech.commons;
 
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
 
 public class AppiumRobot extends BaseTest{
 
+    private TouchAction touchAction;
+
+    public AppiumRobot() {
+        touchAction = new TouchAction(driver);
+    }
+
     public void clicarPorTexto(String texto){
-       driver.findElement(By.xpath("//*[@text='"+texto+"']")).click();
+        driver.findElement(By.xpath("//*[@text='"+texto+"']")).click();
     }
 
     public boolean validaExistenciaTexto(String texto){
         //driverWait.until(ExpectedConditions.textToBePresentInElement(driver.findElement(By.xpath("//android.widget.TextView[@text='"+texto+"']")), texto));
 
         try {
-            Thread.sleep(5000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -21,10 +29,19 @@ public class AppiumRobot extends BaseTest{
 
     public boolean validaExistenciaTextoNaView(String texto){
         try {
-            Thread.sleep(5000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return driver.findElement(By.xpath("//android.view.View[@text='"+texto+"']")).isDisplayed();
+    }
+
+    public void swipeHorizontal(Integer quantidade, Integer xOrigem, Integer xDestino) {
+        for (int i = 0; i < quantidade; i++) {
+            touchAction.press(new PointOption().withCoordinates(xOrigem, 500))
+                    .moveTo(new PointOption().withCoordinates(xDestino, 500))
+                    .release()
+                    .perform();
+        }
     }
 }
--- src/test/java/br/com/treinamento/etech/Funcionalidade/AbasFuncionalidade.java
+++ src/test/java/br/com/treinamento/etech/Funcionalidade/AbasFuncionalidade.java
@@ -1,31 +1,22 @@
 package br.com.treinamento.etech.Funcionalidade;
 
-import br.com.treinamento.etech.Pages.AbasPage;
+import br.com.treinamento.etech.commons.AppiumRobot;
 import br.com.treinamento.etech.commons.BaseTest;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.touch.offset.PointOption;
 
 public class AbasFuncionalidade extends BaseTest {
 
-    private AbasPage abasPage;
-    private TouchAction touchAction;
+    private AppiumRobot appiumRobot;
 
     public AbasFuncionalidade() {
-        touchAction = new TouchAction(driver);
+        appiumRobot = new AppiumRobot();
     }
 
     public void arrastarTelaParaDireita() {
-        touchAction.press(new PointOption().withCoordinates(100, 500))
-                .moveTo(new PointOption().withCoordinates(900, 500))
-                .release()
-                .perform();
+        appiumRobot.swipeHorizontal(1, 100, 900);
     }
 
     public void arrastarTelaParaEsquerda() {
-        touchAction.press(new PointOption().withCoordinates(900, 500))
-                .moveTo(new PointOption().withCoordinates(100, 500))
-                .release()
-                .perform();
+        appiumRobot.swipeHorizontal(1, 900, 100);
     }
 
 }
